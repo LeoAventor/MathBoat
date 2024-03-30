3ee6e608115f924c14e0bc6a957a88bea72aafd2
artursMaslenkovs <arturs.maslenkovs@gmail.com>
2024-03-30 23:23:47 +0200

    Changes

--- game_rules.py
+++ game_rules.py
@@ -12,12 +12,34 @@ class LOGIC_CONTROLLER:
         for i in self.SINGLE_PLAYER_KEYS:
             self.render_data[i] = "?"
 
+        self.render_data["currentStreak"] = 0
+        self.render_data["currentLevel"] = 0
+        self.render_data["currentCount"] = 3
+
     def check_result(self, user_input):
-        print(user_input)
-        print(self.render_data)
-        print(self.puzzle.puzzle_data)
+        print(user_input, self.puzzle.puzzle_data["correctAnswer"])
+        if user_input == "":
+            self.render_data["correctStatus"] = "Please enter answer!"
+        elif user_input == self.puzzle.puzzle_data["correctAnswer"]:
+            self.render_data["correctStatus"] = "Correct"
+            self.render_data["currentStreak"] += 1
+        else:
+            if self.render_data["currentCount"] == 1:
+                self.render_data["correctStatus"] = "Game Over!"
+                self.render_data["currentStreak"] = 0
+                self.render_data["currentLevel"] = 0
+                self.render_data["currentCount"] = 3
+            else:
+                self.render_data["correctStatus"] = "Incorrect"
+                self.render_data["currentStreak"] = 0
+                self.render_data["currentCount"] -= 1
 
     def set_initial_data(self):
-        self.puzzle.generate_new_math_puzzle(exercise=1)
+        self.get_new_puzzle(1,"easy")
+
+    def get_new_puzzle(self, exercise, difficulity):
+        self.puzzle.generate_new_math_puzzle(exercise=exercise, difficulty=difficulity)
         for i in self.puzzle.puzzle_data.keys():
             self.render_data[i] = self.puzzle.puzzle_data[i]
+            #
+            self.render_data.re = self.puzzle.puzzle_data.result_number
---
+++ puzzle_data.py
@@ -0,0 +1,22 @@
+from random import randint
+
+
+class PUZZLE_DATA:
+    first_number = 0
+    sign_symbol = "+"
+    second_number = 0
+    equality_symbol = "="
+    result_number = 0
+    correct_answer = 0
+
+    def generate_new_first_number(self, to):
+        self.first_number = randint(0, to)
+
+    def generate_new_second_number(self, to):
+        self.first_number = randint(1, to)
+
+    def generate_new_math_sign(self):
+        self.sign_symbol = ["+", "-", "*", "/"][randint(0, 3)]
+
+    def convert_to_str(self):
+        self.first_number = str(self.first_number)
