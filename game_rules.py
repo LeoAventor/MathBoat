9f73e1a990ec791aa5faf3eba3ca6803ce1eb732
LeoAventor <grandizer65@gmail.com>
2024-03-31 13:07:25 +0300

    commit

--- game_rules.py
+++ game_rules.py
@@ -11,11 +11,32 @@ class LOGIC_CONTROLLER:
     def __init__(self):
         for i in self.SINGLE_PLAYER_KEYS:
             self.render_data[i] = "?"
+        
+        else:
+            if i =="currentStreak":
+                self.render_data[i] = 0
+            if i =="currentCount":
+                self.render_data[i] = 0
+             
 
     def check_result(self, user_input):
-        print(user_input)
-        print(self.render_data)
-        print(self.puzzle.puzzle_data)
+        expected_result = self.puzzle.puzzle_data["resultNumber"]
+        user_input = int(user_input)
+
+        if user_input == expected_result:
+            self.render_data["correctStatus"] = "Correct"
+            self.render_data["currentStreak"] += 1
+            self.render_data["currentLevel"] += 1
+            self.render_data["currentCount"] = 1
+
+
+        else:
+            self.render_data["correctStatus"] = "Incorrect"
+            self.render_data["currentStreak"] = 0
+            self.render_data["currentCount"] += 1
+            
+
+
 
     def set_initial_data(self):
         self.puzzle.generate_new_math_puzzle(exercise=1)
